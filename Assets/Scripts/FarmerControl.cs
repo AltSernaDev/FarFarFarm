85889bb555660ba342fb445fbc18ffa439a0563e
AltSernaDev <altsernadev@gmail.com>
2023-03-27 10:57:43 -0500

    Project

---
+++ Assets/Scripts/FarmerControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmerControl : MonoBehaviour, ISelectable
+{
+    private void Start()
+    {
+        // Registro de farmer en array
+        for (int i = 0; i < FarmersManager.farmerCtrl.Length; i++)
+        {
+            if (FarmersManager.farmerCtrl[i] != this && FarmersManager.farmerCtrl[i] == null)
+            {
+                FarmersManager.farmerCtrl[i] = this;
+            }
+        }
+    }
+
+    public void Select()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] float maxRange = 3.5f;
+    [SerializeField] LayerMask mask;
+    //Transform selectableTemp;
+    void Update()
+    {
+        Selecting();
+    }
+    void Selecting()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxRange, mask))
+        {
+            var selectable = hit.transform;
+            ISelectable selectableCode = selectable.gameObject.GetComponent<ISelectable>();
+            if (selectableCode != null)
+            {
+                //Hover action
+                //selectableCode.Hover();
+
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    selectableCode.Select();
+                }
+            }
+        }
+    }
+}
